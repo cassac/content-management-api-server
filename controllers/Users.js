6b92bd4f86189eee3411052f2e344dbe151572d1
cassac <hicassac@gmail.com>
2016-11-16 13:38:25 -0800

    user controller basic functionality

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,22 +1,29 @@
 const User = require('../models/Users');
 
 module.exports = {
-  get: (req, res) => {
+  get: (req, res, next) => {
     const userId = req.params.userId;
     if (userId) {
       // logic for individual user
+      User.findOne({id: userId}, (err, foundUser) => {
+        if (err) return next(err);
+        if (foundUser) {
+          return res.json(foundUser)
+        }
+        return res.status(404).json({success: false, message: 'User not found.'})
+      })
       res.json({success: true, message: `GET user: ${userId}`});
     } else {
       // logic to get all users
       res.json({success: true});
     }
   },
-  post: (req, res) => {
+  post: (req, res, next) => {
     const user = req.body;
     // Create user logic
     res.json({success: true});
   },
-  put: (req, res) => {
+  put: (req, res, next) => {
     const userId = req.params.userId;
     if (userId) {
       // logic for individual user
@@ -25,7 +32,7 @@ module.exports = {
       res.json({success: true});
     }    
   },
-  delete: (req, res) => {
+  delete: (req, res, next) => {
     const userId = req.params.userId
     if (userId) {
       // logic for individual user
