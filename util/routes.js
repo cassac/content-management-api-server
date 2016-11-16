d69e64c703fa1051bbbd93343b08a1b96f5df49f
cassac <hicassac@gmail.com>
2016-11-16 15:47:17 -0800

    edits route

--- util/routes.js
+++ util/routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.get('/users', UsersController.get);
 router.post('/users', UsersController.post);
-router.delete('/users', UsersController.delete);
+router.delete('/users/:userId', UsersController.delete);
 router.get('/users/:userId', UsersController.get);
 router.put('/users/:userId', UsersController.put);
 
