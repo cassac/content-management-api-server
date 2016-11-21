3551cfd7f187fc66ee92ac130d45f5d24e1f9ba3
cassac <hicassac@gmail.com>
2016-11-20 16:05:35 -0800

    connects bluebird promises to mongoose

--- models/Users.js
+++ models/Users.js
@@ -1,22 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+mongoose.Promise = require('bluebird');
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false  },
   firstName: String,
   lastName: String,
   companyName: String
 })
 
-userSchema.pre('save', (next) => {
-  const user = this;
-  
-  console.log('user:', user);
+// userSchema.pre('save', (next) => {
+//   const user = this;
 
-  // logic to hash and save password
+//   console.log('user:', user);
 
-})
+//   // logic to hash and save password
+
+// })
 
 const UserModel = mongoose.model('user', userSchema);
 
