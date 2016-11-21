f2720053f8f7fe800d27907528720e0437ed41d1
cassac <hicassac@gmail.com>
2016-11-20 16:59:11 -0800

    refactors test to test user with test db

--- test/users.js
+++ test/users.js
@@ -2,18 +2,32 @@ const chai = require('chai');
 const assert = chai.assert;  
 const expect = chai.expect;  
 const should = chai.should();
-
+const mongoose = require('mongoose');
 const User = require('../models/Users.js');
 
-const fakeUser = {email: 'abc@123.com', password: '123'};
+mongoose.connect('mongodb://localhost:graphics_test');
+
+const fakeUser1 = {email: 'abcdefg@123.com', password: '123'};
+const fakeUser2 = {email: 'efg@123.com', password: '123'};
 
 describe('User model', () => {
 
-  it('user should be an instance of User', () => {
-    user = new User(fakeUser)
-      .save(err => {
-        if (err) throw err;
-        expect(user instanceof User).to.equal(true);
+  before(done => {    
+    const user1 = new User(fakeUser1);
+    const user2 = new User(fakeUser2);
+    user1.save();
+    user2.save(() => done() );    
+  });  
+
+  after(done => {    
+    User.remove({}, () => done() );  
+  });
+
+  it('User collection should have two users', (done) => {
+    User.find({})
+      .then(users => {
+        users.should.have.length(2);
+        done();
       });
   });
 
@@ -22,6 +36,13 @@ describe('User model', () => {
       .save(err => {
         expect(err).should.have.property('ValidationError');
       });
-  })
+  });
+
+  it('User should encrypt password on save', (done) => {
+    User.findOne({}).select('+password').exec((err, user) => {
+      user.password.should.not.equal('123')
+      done();
+    });
+  });
 
 });
