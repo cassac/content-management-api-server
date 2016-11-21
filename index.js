a7187a579e2dd17b2ffd10952bde95da3d471b7b
cassac <hicassac@gmail.com>
2016-11-20 21:22:44 -0800

    imports

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const path = require('path');
 const mongoose = require('mongoose');
-const port = require('./util/config').port;
+const path = require('path');
+const config = require('./util/config');
 const router = require('./util/routes');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:graphics');
+mongoose.connect(config.dbUri);
 
 app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(bodyParser.json({ type: '*/*' }));
@@ -18,6 +18,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
 })
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+app.listen(config.port, () => {
+  console.log(`App running on port ${config.port}`)
 })
+
+module.exports = app;
