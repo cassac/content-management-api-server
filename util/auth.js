abca46736117e6a0388621bab6e5c58620bc0763
cassac <hicassac@gmail.com>
2016-11-30 15:29:24 -0800

    adds admin required middleware helper

--- util/auth.js
+++ util/auth.js
@@ -5,7 +5,14 @@ const User = require('../models/users');
 module.exports = {
   grantUserToken: (user) => {
     const timestamp = new Date().getTime();
-    const data = {sub: user._id, admin: user.isAdmin, iat: timestamp};
+    const data = {sub: user._id, isAdmin: user.isAdmin, iat: timestamp};
     return jwt.encode(data, secretKey);
   },
+  requireAdmin: (req, res, next) => {
+    const { isAdmin } = jwt.decode(req.headers.authorization, secretKey);
+    if (!isAdmin) {
+      return res.status(403).json({success: false, message: 'Unauthorized.', results: [] });
+    }
+    return next();
+  }
 }
