299c23b7c6dc032575e9c26c2f3a5709a99e4be6
cassac <hicassac@gmail.com>
2016-11-30 14:27:14 -0800

    connects auth middleware

--- util/routes.js
+++ util/routes.js
@@ -3,7 +3,15 @@ const AuthController = require('../controllers/auth');
 const apiRouter = require('express').Router();
 const authRouter = require('express').Router();
 
-apiRouter.get('/users', UsersController.get);
+const passport = require('passport');
+const passportService = require('../services/passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+// TODO
+// add requireAdmin middleware
+
+apiRouter.get('/users', requireAuth, UsersController.get);
 apiRouter.post('/users', UsersController.post);
 apiRouter.delete('/users/:userId', UsersController.delete);
 apiRouter.get('/users/:userId', UsersController.get);
