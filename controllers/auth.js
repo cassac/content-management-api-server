814a86b85897f8356a44b0b1e3e17e512c98b4de
cassac <hicassac@gmail.com>
2016-11-30 14:04:10 -0800

    completes signup logic and responses

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,24 +3,24 @@ const util = require('../util/auth');
 
 module.exports = {
  signup: (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
   if (username && password) {
     User.findOne({username})
       .then(existingUser => {
-        console.log('existingUser:', existingUser)
         if (!existingUser) {
           const user = new User(req.body);
           user.save()
             .then(user => {
-              // TODO
-              // send auth token
-              const data = {success: true, message: 'User created.', results: user};
+              const token = util.grantUserToken(user);
+              // Remove sensitive information before sending to client
+              user = user.toObject();
+              delete user.password;
+              delete user.isAdmin;
+              const data = {token, success: true, message: 'User created.', results: user};
               return res.status(201).json(data);
             })
             .catch(err => {
-              console.log('err user:', err)
-              return next(err);
+              return res.status(500).json({success: false, message: `User creation error: ${err.message}`, results: [] });
             });
         }
         else {
@@ -28,32 +28,34 @@ module.exports = {
         }
       })
       .catch(err => {
-        console.log('not found:', err)
-        return next(err);
+        return res.status(500).json({success: false, message: `Signup error: ${err.message}`, results: [] });
       })
   } 
   else {
-    console.log('must provide username and password')
+    return res.status(400).json({success: false, message: 'Signup requires username and password.', results: [] });
   }
  },
- signin: (req, res, next) => {
+ signin: (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     return res.status(400).json({success: false, message: 'Must provide username and password.', results: [] });
   } 
-  User.findOne({username}).select('+password').exec()
-      .then(user => {
-        if (!user) {
-          return res.status(404).json({success: false, message: 'User not found.', results: [] });
+  User.findOne({username}).select('+password +isAdmin').exec()
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({success: false, message: 'User not found.', results: [] });
+      }
+      user.comparePassword(password, function(err, isMatch) {
+        if (err) return err;
+        if (!isMatch) {
+          return res.status(400).json({success: false, message: 'Incorrect username and/or password', results: [] });
         }
-        user.comparePassword(password, function(err, isMatch) {
-          if (err) return err;
-          if (!isMatch) {
-            return res.status(400).json({success: false, message: 'Incorrect username and/or password', results: [] });
-          }
-          let token = '1234'; // generate real token
-          return res.status(200).json({token, success: true, message: 'Successful login.', results: []})
-        });
-      })
- } 
+        const token = util.grantUserToken(user);
+        return res.status(200).json({token, success: true, message: 'Successful login.', results: []})
+      });
+    })
+    .catch(err => {
+      return res.status(500).json({success: false, message: `Signin error: ${err.message}`, results: [] });
+    })
+  } 
 }
