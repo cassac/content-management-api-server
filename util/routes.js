1c772cbc12cbf42bdd6f870d9083bb649e36650a
cassac <hicassac@gmail.com>
2016-11-30 21:48:46 -0800

    applies middleware

--- util/routes.js
+++ util/routes.js
@@ -1,19 +1,19 @@
 const UsersController = require('../controllers/users');
 const AuthController = require('../controllers/auth');
-const util = require('./auth');
 const apiRouter = require('express').Router();
 const authRouter = require('express').Router();
 
 const passport = require('passport');
 const passportService = require('../services/passport');
 
+const { requireUserOrAdmin, requireAdmin } = require('./auth');
 const requireAuth = passport.authenticate('jwt', { session: false });
 
-apiRouter.get('/users', util.requireAdmin, UsersController.get);
-apiRouter.post('/users', UsersController.post);
-apiRouter.delete('/users/:userId', UsersController.delete);
-apiRouter.get('/users/:userId', UsersController.get);
-apiRouter.put('/users/:userId', UsersController.put);
+apiRouter.get('/users', requireAuth, requireAdmin, UsersController.get);
+apiRouter.post('/users', requireAuth, requireAdmin, UsersController.post);
+apiRouter.delete('/users/:userId', requireAuth, requireAdmin, UsersController.delete);
+apiRouter.get('/users/:userId', requireAuth, requireUserOrAdmin, UsersController.get);
+apiRouter.put('/users/:userId', requireAuth, requireUserOrAdmin, UsersController.put);
 
 authRouter.post('/signin', AuthController.signin);
 authRouter.post('/signup', AuthController.signup);
