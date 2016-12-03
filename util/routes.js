aaf0dc3f246519fd03cdd1d0c44c1600889a7dae
cassac <hicassac@gmail.com>
2016-12-03 10:03:55 -0800

    routes for file get requests

--- util/routes.js
+++ util/routes.js
@@ -1,4 +1,5 @@
 const UsersController = require('../controllers/users');
+const FilesController = require('../controllers/files');
 const AuthController = require('../controllers/auth');
 const apiRouter = require('express').Router();
 const authRouter = require('express').Router();
@@ -14,6 +15,10 @@ apiRouter.post('/users', requireAuth, requireAdmin, UsersController.post);
 apiRouter.delete('/users/:userId', requireAuth, requireAdmin, UsersController.delete);
 apiRouter.get('/users/:userId', requireAuth, requireUserOrAdmin, UsersController.get);
 apiRouter.put('/users/:userId', requireAuth, requireUserOrAdmin, UsersController.put);
+
+apiRouter.get('/users/files', requireAuth, requireAdmin, FilesController.get);
+apiRouter.get('/users/:userId/files', requireAuth, requireUserOrAdmin, FilesController.get);
+apiRouter.get('/files/:fileId', requireAuth, requireUserOrAdmin, FilesController.get);
 
 authRouter.post('/signin', AuthController.signin);
 authRouter.post('/signup', AuthController.signup);
