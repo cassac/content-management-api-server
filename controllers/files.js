7a12e04df29bf87cf9f3d895d650be83b4b869b0
cassac <hicassac@gmail.com>
2016-12-03 09:53:36 -0800

    basic files controller

---
+++ controllers/files.js
@@ -0,0 +1,16 @@
+const File = require('../models/files');
+
+module.exports = {
+  get: (req, res, next) => {
+
+  },
+  post: (req, res, next) => {
+
+  },
+  put: (req, res, next) => {
+
+  },
+  delete: (req, res, next) => {
+
+  },
+}
