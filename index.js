ae90d758cbafc4acfe88b49e94de75616049cf03
cassac <hicassac@gmail.com>
2016-12-04 12:42:33 -0800

    clean up

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -11,7 +10,6 @@ const app = express();
 mongoose.connect(config.dbUri);
 
 app.use(express.static(path.join(__dirname, '../client/public')));
-// app.use(bodyParser.json({ type: '*/*' }));
 app.use('/api', router.apiRouter);
 app.use('/auth', router.authRouter);
 
