d1f87045e38c5cb0178554255bba19fb0cb9856a
cassac <hicassac@gmail.com>
2016-12-04 10:19:42 -0800

    adds passport local login strategy

---
+++ services/passport.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const User = require('../models/users');
+const config = require('../util/config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secretKey
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);
