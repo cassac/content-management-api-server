4566e5baa1354e85e89a6d0f9354b78621266a57
cassac <hicassac@gmail.com>
2016-12-04 15:22:39 -0800

    defines allowed file types. separates upload path and upload dir

--- util/config.js
+++ util/config.js
@@ -6,14 +6,25 @@ module.exports = {
   dbUri: process.env.TESTING ? 
     'mongodb://localhost:db/graphics_testing' :
     'mongodb://localhost:db/graphics',
-  uploadDir: (filename) => {
+  uploadPath: (filename) => {
     const date = new Date();
     return path.join(
-      __dirname, 
-      '../uploads/', 
       date.getFullYear().toString(), 
       (date.getMonth() + 1).toString(),
       filename
     );
   },
+  uploadDir: path.join(__dirname, '../uploads'),
+  allowedFileTypes: [
+    // Images
+    'jpg', 'jpeg', 'png', 'gif',
+    // Documents
+    'pdf', 'doc', 'docx', 'key', 'ppt', 'pptx','pps',
+    'ppsx', 'odt', 'xls', 'xlsx', 'zip',
+    // Audio
+    'mp3', 'm4a', 'ogg', 'wav',
+    // Video
+    'mp4', 'm4v', 'mov', 'wmv', 'avi', 'mpg', 'ogv',
+    '3gp', '3g2',
+  ],
 }
