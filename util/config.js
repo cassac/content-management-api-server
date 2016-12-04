e4987b5292853ef6d25175d3acab71b49acbfbee
cassac <hicassac@gmail.com>
2016-12-04 12:42:17 -0800

    creates dynamic upload dir

--- util/config.js
+++ util/config.js
@@ -1,7 +1,19 @@
+const path = require('path');
+
 module.exports = {
   secretKey: 'this needs to be more secret',
   port: process.env.PORT || 3000,
   dbUri: process.env.TESTING ? 
     'mongodb://localhost:db/graphics_testing' :
     'mongodb://localhost:db/graphics',
+  uploadDir: (filename) => {
+    const date = new Date();
+    return path.join(
+      __dirname, 
+      '../uploads/', 
+      date.getFullYear().toString(), 
+      (date.getMonth() + 1).toString(),
+      filename
+    );
+  },
 }
